9eac38354adf5a1ae5f92e5aca0f008622f393ff
David Ingram <david@ingramaviation.com>
2019-07-16 19:01:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,17 @@
 def addition(num1, num2)
-  num1 = .to_f 
-  num2 = .to_f
-  puts (num1 + num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
 
 def division(num1, num2)
-
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1  * num2
 end
 
 def modulo(num1, num2)
